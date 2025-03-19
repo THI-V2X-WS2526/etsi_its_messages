692acd39fe31e33201393656f918132d08f22915
Silas Lobo <silascorreia.lobo@carissma.eu>
2025-03-19 12:39:09 +0100

    added mcm msg

---
+++ etsi_its_coding/etsi_its_mcm_thi_prima_coding/src/per_decoder.c
@@ -0,0 +1,5 @@
+#include <etsi_its_mcm_thi_prima_coding/asn_application.h>
+#include <etsi_its_mcm_thi_prima_coding/asn_internal.h>
+#include <etsi_its_mcm_thi_prima_coding/per_decoder.h>
+
+// Absolutely nothing
